669640cdd6dd1daf9231329ccba6c5c68fdb9850
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-02-02 22:25:21 +0530

    Updated HomePage

--- movie-app/src/pages/HomePage.jsx
+++ movie-app/src/pages/HomePage.jsx
@@ -3,25 +3,27 @@ import "./HomePage_style.css";
 
 import Sidebar2 from "../components/Sidebar2/Sidebar2";
 import Navbar from "../components/Navbar/Navbar";
+import MovieApp from "../components/MovieCard/MovieApp";
 
-import MovieCard from "../components/MovieCard/MovieCard";
 
-function HomePage() {
+function HomePage()
+{
   const [isScrolled, setIsScrolled] = useState(false);
 
-  window.onscroll = () => {
+  window.onscroll = () =>
+  {
     setIsScrolled(window.pageYOffset === 0 ? false : true);
     return () => (window.onscroll = null);
   };
 
-  const [movieData, setMovieData] = useState();
-  console.log();
 
   return (
     <div className="home_page">
       <Sidebar2 />
-      <Navbar isScrolled={isScrolled} />
-      <MovieCard />
+      
+      <div className="container" >
+        <MovieApp />
+      </div> 
     </div>
   );
 }
