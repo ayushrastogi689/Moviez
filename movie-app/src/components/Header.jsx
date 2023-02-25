668aa95b6c326610ec67a9acf618c484e3cacf69
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-02-25 05:50:42 +0000

    Updated header

--- movie-app/src/components/Header.jsx
+++ movie-app/src/components/Header.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-
 export default function Header(props) {
   const navigate = useNavigate();
   return (
     <StyledHeader className="flex a-center j-between">
-      <div className="logo">
-
-      </div>
+      <div className="logo"></div>
       <button onClick={() => navigate(props.login ? "/login" : "/signup")}>
         {props.login ? "Log In" : "Sign In"}
       </button>
